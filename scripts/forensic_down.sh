fdda2cd86e3b1f36f8ea041ee7b444a1a9ed8be0
Carlos Polop <carlospolop@gmail.com>
2019-11-27 11:18:45 +0000

    Update forensic_down.sh

--- scripts/forensic_down.sh
+++ scripts/forensic_down.sh
@@ -11,7 +11,7 @@ write_main "Installing clamAV"
 apt-get install -y clamav
 
 write_main "Installing suricata"
-apt-get install suricata
-apt-get install oinkmaster
+apt-get install -y suricata
+apt-get install -y oinkmaster
 echo "url = http://rules.emergingthreats.net/open/suricata/emerging.rules.tar.gz" >> /etc/oinkmaster.conf
 oinkmaster -C /etc/oinkmaster.conf -o /etc/suricata/rules
